81e8ec955d8114ef62cf7071b06e6df24ef9e240
Declan Baldwin <declan@myplayer.io>
2022-02-14 21:26:12 +0000

    added form submission

--- components/LandingPageForm.js
+++ components/LandingPageForm.js
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Select } from ".";
 import { useFilteredProjects } from "../hooks";
 const technologies = ["All", "HTML / CSS", "JavaScript", "React"];
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 const LandingPageForm = () => {
   const [email, setEmail] = useState("");
   const [technology, setTechnology] = useState();
   const [project, setProject] = useState();
+  const formRef = useRef();
   const filteredProjects = useFilteredProjects({ technology });
   const selectTechnology = (item) => {
     setProject();
@@ -17,8 +24,35 @@ const LandingPageForm = () => {
     setProject(item);
   };
 
+  const handleSubmit = (e) => {
+    console.log("handleSubmit");
+    e.preventDefault();
+
+    const form = formRef.current;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        email,
+        technology,
+        project,
+      }),
+    })
+      .then(() => console.log("success"))
+      .catch((error) => alert(error));
+  };
+
   return (
-    <div className="flex flex-col pl-16 pr-16">
+    <form
+      name="contact"
+      method="POST"
+      className="flex flex-col pl-16 pr-16"
+      action="/thanks/"
+      data-netlify="true"
+      ref={formRef}
+    >
+      <input type="hidden" name="form-name" value="contact" />
       <div className="font-bold text-xl uppercase mb-6">join</div>
       <div className="text-gray-500 text-base mb-4">
         Start your first project for free.
@@ -51,11 +85,14 @@ const LandingPageForm = () => {
         ></input>
       </div>
       <div className="bg-purple flex items-center justify-center rounded-lg cursor-pointer h-[4.25rem]">
-        <div className="text-white uppercase font-semibold text-base tracking-wider">
+        <button
+          onClick={handleSubmit}
+          className="text-white uppercase font-semibold text-base tracking-wider"
+        >
           join project
-        </div>
+        </button>
       </div>
-    </div>
+    </form>
   );
 };
 export default LandingPageForm;
