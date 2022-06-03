2947250cdce361a19ca86cae947be0b9766e2fe4
declanbaldwin <declanbaldwin@gmail.com>
2022-06-03 12:45:37 +0100

    added layout with menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,75 @@
 import '../styles/globals.css'
 import Image from "next/image";
+import React, { useState } from 'react';
+import { useRouter } from "next/router";
 
-const Layout = () => {
+const links = [
+  {
+    text: 'Home',
+    href: '/'
+  },
+  {
+    text: 'Projects',
+    href: '/projects'
+  },
+  {
+    text: 'Mentors',
+    href: '/mentors'
+  }
+]
+
+const Menu = ({ show, onClose }) => {
+  const router = useRouter();
+  const className = `cursor-pointer text-white rounded-b-lg -translate-x-full origin-top-right bg-black-300 fixed ease-in-out scale-0 duration-150 ${show ? 'scale-100	' : ''}`
+
+  const onLinkClick = (link) => {
+    onClose();
+    router.push(link.href);
+  }
+
   return (
-    <div className='fixed top-0 right-0'>
-      <div className="p-2 w-12 h-12 relative rounded-full mr-4 mt-4 cursor-pointer ease-in-out duration-150 hover:bg-gray-400">
-        <Image
-          src="/menu.svg"
-          layout="responsive"
-          width={24}
-          height={24}
-          alt="menu"
-        />
+    <div className={className}>
+      <div className="relative flex flex-col w-full">
+        {links.map((link, i) => {
+          return (
+            <div
+              key={i}
+              onClick={() => onLinkClick(link)}
+              className="w-[10rem] h-[4.25rem] flex flex-row justify-center items-center hover:bg-black-200"
+            >
+              {link.text}
+            </div>
+          );
+        })}
       </div>
     </div>
+  )
+}
+
+const Layout = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const onClick = () => setShowMenu(!showMenu);
+
+  const onCloseMenu = () => setShowMenu(false)
+
+  return (
+    <>
+      <div className='fixed top-0 right-0'>
+        <div className='relative'>
+          <div className="p-2 w-12 h-12 rounded-full mr-4 mt-4 cursor-pointer ease-in-out duration-150 hover:bg-gray-400" onClick={onClick}>
+            <Image
+              src="/menu.svg"
+              layout="responsive"
+              width={24}
+              height={24}
+              alt="menu"
+            />
+          </div>
+          <Menu show={showMenu} onClose={onCloseMenu} />
+        </div>
+      </div>
+    </>
   )
 }
 
