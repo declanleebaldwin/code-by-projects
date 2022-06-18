e4e894faea4a1e9e1abf95a9df04156a5677ea91
declanbaldwin <declanbaldwin@gmail.com>
2022-06-18 16:56:17 +0100

    add success page back

--- components/LandingPageForm.js
+++ components/LandingPageForm.js
@@ -70,7 +70,7 @@ const LandingPageForm = () => {
         project
       }),
     })
-      .then(() => alert('success'))
+      .then(() => onSubmitSuccess())
       .catch((error) => onSubmitError(error));
   };
 
